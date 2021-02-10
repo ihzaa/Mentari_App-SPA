6f9f272f3083d0fb6831e12d91e34458bfa80782
yusufrnp <yusufrnp@gmail.com>
2021-02-10 10:28:40 +0700

    commit

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,21 +1,21 @@
 <template>
 <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+  <b-navbar toggleable="lg" type="light" class="bg-white" variant="info">
+    <b-navbar-brand class="" href="#"><h3><strong>Mentari</strong></h3></b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#"><h4>Kategori</h4></b-nav-item>
         <b-nav-item href="#" disabled>Disabled</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="lg" class="mr-lg-2" placeholder="Search"></b-form-input>
-          <b-button size="lg" class="my-2 my-lg-0" type="submit">Search</b-button>
+          <b-form-input size="md" class="mr-md-2" placeholder="Search"></b-form-input>
+          <b-button size="md" class="my-2 my-md-0" type="submit">Search</b-button>
         </b-nav-form>
 
         <b-nav-item-dropdown text="Lang" right>
