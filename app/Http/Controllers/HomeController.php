6ab0cb9cb18e24f1447c78230924bfd3dbb2fe3a
ulfanuroktaviana575 <ulfanuroktaviana575@webmail.umm.ac.id>
2021-02-24 09:51:25 +0700

    load product

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,80 +17,80 @@ class HomeController extends Controller
     public function getCarousel()
     {
         $carousel = poster::all();
-        if($carousel != NULL)
-        {
+        if ($carousel != NULL) {
             return response()->json([
                 'message' => 'success',
                 'data' => $carousel
             ], 200);
-        }else{
+        } else {
             return 0;
         }
     }
     public function getProduct(Request $request)
     {
-        if($request->search == "undefined" && $request->category == "undefined"){
-            $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock,categories.id AS category_id, categories.name AS category_name, (SELECT item_images.path FROM item_images WHERE item_images.item_id = items.id ORDER BY item_images.id LIMIT 1) AS path FROM items JOIN categories ON items.category_id = categories.id ORDER BY items.updated_at DESC '));
-        }else if($request->sea){
-
-        }
-        $product = $this->paginateArray(DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock,categories.id AS category_id, categories.name AS category_name, (SELECT item_images.path FROM item_images WHERE item_images.item_id = items.id ORDER BY item_images.id LIMIT 1) AS path FROM items JOIN categories ON items.category_id = categories.id ORDER BY items.updated_at DESC')));
+        if ($request->search == "" && $request->category == "") {
+            $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock,categories.id AS category_id, categories.name AS category_name, (SELECT item_images.path FROM item_images WHERE item_images.item_id = items.id ORDER BY item_images.id LIMIT 1) AS path FROM items JOIN categories ON items.category_id = categories.id WHERE items.id > ' . $request->lastId . ' ORDER BY items.id ASC LIMIT 15'));
+        } else if ($request->search == "") {
+            $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock,categories.id AS category_id, categories.name AS category_name, (SELECT item_images.path FROM item_images WHERE item_images.item_id = items.id ORDER BY item_images.id LIMIT 1) AS path FROM items JOIN categories ON items.category_id = categories.id WHERE items.id > ' . $request->lastId . ' AND category_id = ' . $request->category . '  ORDER BY items.id ASC LIMIT 15 '));
+        } else if ($request->category == "") {
+            $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock,categories.id AS category_id, categories.name AS category_name, (SELECT item_images.path FROM item_images WHERE item_images.item_id = items.id ORDER BY item_images.id LIMIT 1) AS path FROM items JOIN categories ON items.category_id = categories.id WHERE items.id > ' . $request->lastId . ' AND items.name LIKE "%' . $request->search . '%"  ORDER BY items.id ASC LIMIT 15 '));
+        } else
+            $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock,categories.id AS category_id, categories.name AS category_name, (SELECT item_images.path FROM item_images WHERE item_images.item_id = items.id ORDER BY item_images.id LIMIT 1) AS path FROM items JOIN categories ON items.category_id = categories.id WHERE items.id > ' . $request->lastId . ' AND items.name LIKE "%' . $request->search . '%"  AND category_id = ' . $request->category . '  ORDER BY items.id ASC LIMIT 15 '));
 
         // dd($product);
-        if($product != NULL)
-        {
+        if ($product != NULL) {
             return response()->json([
                 'message' => 'success',
                 'data' => $product
             ], 200);
-        }else{
-            return 0;
+        } else {
+            return response()->json([
+                'message' => 'failure',
+                'data' => []
+            ], 200);
         }
     }
     public function getDetail($id)
     {
-        $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock, categories.name AS category_name FROM items JOIN categories ON items.category_id = categories.id WHERE items.id = '.$id));
+        $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock, categories.name AS category_name FROM items JOIN categories ON items.category_id = categories.id WHERE items.id = ' . $id));
 
-        if($product != NULL)
-        {
+        if ($product != NULL) {
             return response()->json([
                 'message' => 'success',
                 'data' => $product
             ], 200);
-        }else{
+        } else {
             return 0;
         }
     }
 
     public function getDetailImage($id)
     {
-        $img = item_image::where('item_id',$id)->get(['id','path']);
+        $img = item_image::where('item_id', $id)->get(['id', 'path']);
 
-        if($img != NULL)
-        {
+        if ($img != NULL) {
             return response()->json([
                 'message' => 'success',
                 'data' => $img
             ], 200);
-        }else{
+        } else {
             return 0;
         }
     }
     public function getCategory()
     {
-        $category = category::get(['id','name']);
-        if($category != NULL)
-        {
+        $category = category::get(['id', 'name']);
+        if ($category != NULL) {
             return response()->json([
                 'message' => 'success',
                 'data' => $category
             ], 200);
-        }else{
+        } else {
             return 0;
         }
     }
 
-     public function paginateArray($data, $perPage = 15)
+    public function paginateArray($data, $perPage = 15)
     {
         $page = Paginator::resolveCurrentPage();
         $total = count($data);
@@ -103,21 +103,20 @@ class HomeController extends Controller
 
     public function getProductName(Request $request)
     {
-        if($request->name == 'undefined'){
-           return $this->getProduct();
-        }else{
-            $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock,categories.id AS category_id, categories.name AS category_name, (SELECT item_images.path FROM item_images WHERE item_images.item_id = items.id ORDER BY item_images.id LIMIT 1) AS path FROM items JOIN categories ON items.category_id = categories.id WHERE items.name LIKE "%'.$request->name.'%" ORDER BY items.updated_at DESC'));
+        if ($request->name == 'undefined') {
+            return $this->getProduct();
+        } else {
+            $product = DB::select(DB::raw('SELECT items.id AS item_id,items.name,items.price,items.description,items.stock,categories.id AS category_id, categories.name AS category_name, (SELECT item_images.path FROM item_images WHERE item_images.item_id = items.id ORDER BY item_images.id LIMIT 1) AS path FROM items JOIN categories ON items.category_id = categories.id WHERE items.name LIKE "%' . $request->name . '%" ORDER BY items.updated_at DESC'));
         };
 
 
         // dd($product);
-        if($product != NULL)
-        {
+        if ($product != NULL) {
             return response()->json([
                 'message' => 'success',
                 'data' => $product
             ], 200);
-        }else{
+        } else {
             return 0;
         }
     }
--- resources/js/components/Product.vue
+++ resources/js/components/Product.vue
@@ -1,109 +1,103 @@
 <style scoped>
 .product-card {
-    max-width: 20rem;
+  max-width: 20rem;
 }
 .product-card:hover {
-    cursor: pointer;
-    box-shadow: 0 8px 16px 0 lightgreen;
+  cursor: pointer;
+  box-shadow: 0 8px 16px 0 lightgreen;
 }
 .header {
-    background-color: white;
+  background-color: white;
 }
 .info {
-    height: 110px;
+  height: 110px;
 }
 .harga {
-    font-size: 18px;
-    font-weight: 1000 !important;
+  font-size: 18px;
+  font-weight: 1000 !important;
 }
 .card-body {
-    padding: 0;
+  padding: 0;
 }
 .card-footer {
-    background-color: white;
+  background-color: white;
 }
 @media only screen and (max-width: 700px) {
-    .product-card {
-        max-width: 40rem;
-    }
+  .product-card {
+    max-width: 40rem;
+  }
 }
 </style>
 
 <template>
-    <div class="mx-5 my-3">
-        <b-row class="justify-content-around" align-v="center">
-            <b-card
-                class="product-card my-2 p-3 mx-1"
-                v-for="product in productInfo"
-                @click="goToDetail(product.item_id)"
-                v-bind:key="product.item_id"
-                name=""
-                data-aos="zoom-in-up"
-            >
-                <b-card-img-lazy
-                    width="100%"
-                    height="200"
-                    v-if="product.path != null"
-                    v-lazy="global + product.path"
-                    top
-                ></b-card-img-lazy>
-                <b-card-img-lazy
-                    width="100%"
-                    height="200"
-                    v-else
-                    src="/frontend/images/no-image-available.png"
-                    style="border: 1px solid lightgray"
-                    top
-                ></b-card-img-lazy>
-                <b-card-header class="header text-center" tag="header">
-                    <h5>
-                        <strong>{{ product.name }}</strong>
-                    </h5>
-                    <p style="margin-bottom: -10px">
-                        {{ product.category_name }}
-                    </p>
-                </b-card-header>
-                <b-card-body>
-                    <b-card-text
-                        class="info"
-                        v-if="product.description.length < 180"
-                    >
-                        {{ product.description }}
-                    </b-card-text>
-                    <b-card-text class="info" v-else>
-                        {{ product.description.substring(0, 180) + "..." }}
-                    </b-card-text>
-                    <b-card-text class="harga text-right">
-                        <p>Rp. {{ formatPrice(product.price) }}</p>
-                    </b-card-text>
-                </b-card-body>
-                <b-card-footer class="card-footer" tag="footer">
-                    <b-button
-                        @click.stop="addToCart(product.item_id)"
-                        variant="success"
-                    >
-                        <b-cart-check-fill style="width: 20px; height: 20px">
-                        </b-cart-check-fill>
-                    </b-button>
-                </b-card-footer>
-            </b-card>
-        </b-row>
-        <div class="text-center my-4" v-show="moreExists">
-            <b-button variant="success" @click="loadMore" v-if="!loading">
-                Tampilkan Lebih Banyak
-            </b-button>
-            <b-button
-                variant="success"
-                style="font-size:16px"
-                disabled
-                align-v="center"
-                v-else
-            >
-                <b-spinner small></b-spinner>
-                Loading...
-            </b-button>
-        </div>
+  <div class="mx-5 my-3">
+    <b-row class="justify-content-around" align-v="center">
+      <b-card
+        class="product-card my-2 p-3 mx-1"
+        v-for="product in productInfo"
+        @click="goToDetail(product.item_id)"
+        v-bind:key="product.item_id"
+        name=""
+        data-aos="zoom-in-up"
+      >
+        <b-card-img-lazy
+          width="100%"
+          height="200"
+          v-if="product.path != null"
+          v-lazy="global + product.path"
+          top
+        ></b-card-img-lazy>
+        <b-card-img-lazy
+          width="100%"
+          height="200"
+          v-else
+          src="/frontend/images/no-image-available.png"
+          style="border: 1px solid lightgray"
+          top
+        ></b-card-img-lazy>
+        <b-card-header class="header text-center" tag="header">
+          <h5>
+            <strong>{{ product.name }}</strong>
+          </h5>
+          <p style="margin-bottom: -10px">
+            {{ product.category_name }}
+          </p>
+        </b-card-header>
+        <b-card-body>
+          <b-card-text class="info" v-if="product.description.length < 180">
+            {{ product.description }}
+          </b-card-text>
+          <b-card-text class="info" v-else>
+            {{ product.description.substring(0, 180) + "..." }}
+          </b-card-text>
+          <b-card-text class="harga text-right">
+            <p>Rp. {{ formatPrice(product.price) }}</p>
+          </b-card-text>
+        </b-card-body>
+        <b-card-footer class="card-footer" tag="footer">
+          <b-button @click.stop="addToCart(product.item_id)" variant="success">
+            <b-cart-check-fill style="width: 20px; height: 20px">
+            </b-cart-check-fill>
+          </b-button>
+        </b-card-footer>
+      </b-card>
+    </b-row>
+    <div class="text-center my-4" v-show="moreExists">
+      <b-button variant="success" @click="loadMore" v-if="!loading">
+        Tampilkan Lebih Banyak
+      </b-button>
+      <b-button
+        variant="success"
+        style="font-size: 16px"
+        disabled
+        align-v="center"
+        v-else
+      >
+        <b-spinner small></b-spinner>
+        Loading...
+      </b-button>
     </div>
+  </div>
 </template>
 
 <script>
@@ -115,122 +109,135 @@ import { EventBus } from "../event-bus";
 AOS.init();
 
 export default {
-    data() {
-        return {
-            searchvalue: "",
-            categoryvalue: "",
-            productInfo: [],
-            busy: false,
-            limit: 8,
-            global: window.Global.imgPath,
-            moreExists: true,
-            nextPage: 0,
-            loading: false,
-            searchProduct: false
-        };
+  data() {
+    return {
+      searchvalue: "",
+      categoryvalue: "",
+      productInfo: [],
+      busy: false,
+      limit: 8,
+      global: window.Global.imgPath,
+      moreExists: true,
+      nextPage: 0,
+      loading: false,
+      searchProduct: false,
+      lastProductId: 0,
+    };
+  },
+  methods: {
+    formatPrice(value) {
+      let val = (value / 1).toFixed(0).replace(".", ",");
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     },
-    methods: {
-        formatPrice(value) {
-            let val = (value / 1).toFixed(0).replace(".", ",");
-            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-        },
-        goToDetail(proId) {
-            this.$router.push({ name: "detail", params: { id: proId } });
-        },
-        loadProduct: async function() {
-            try {
-                let response = await axios.post(`/api/product`, {
-                    search: this.searchvalue,
-                    category: this.categoryvalue
-                });
-                this.productInfo = response.data.data.data;
-
-                // if (
-                //     response.data.data.current_page <
-                //     response.data.data.last_page
-                // ) {
-                //     this.moreExists = true;
-                //     this.nextPage = response.data.data.current_page + 1;
-                // } else {
-                //     this.moreExists = false;
-                // }
-            } catch (err) {
-                console.log(err);
-            }
-        },
-        addToCart(id) {
-            if (_.isEmpty(User.data)) {
-                this.$router.push({ name: "login" });
-            } else {
-                this.$root.$refs.Loading.show();
-
-                axios
-                    .get(window.Global.baseUrl + "/api/add/to/cart/" + id)
-                    .then(resp => {
-                        this.$root.$refs.Nav.cartCounter = resp.data;
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    })
-                    .finally(() => {
-                        this.$root.$refs.Loading.hide();
-                    });
-            }
-        },
-        loadMore: async function() {
-            this.loading = true;
-            await axios
-                .get(`/api/product?page=` + this.nextPage)
-                .then(response => {
-                    console.log(response);
-
-                    response.data.data.data.forEach(data => {
-                        this.productInfo.push(data);
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-                .finally(() => {
-                    this.loading = false;
-                });
-        },
-        filteredProduct() {
-            this.searchProduct = true;
-            axios
-                .get(`/api/search?name=` + this.seachvalue)
-                .then(response => {
-                    if (
-                        response.data.data.current_page <
-                        response.data.data.last_page
-                    ) {
-                        this.moreExists = true;
-                        this.nextPage = response.data.data.current_page + 1;
-                    } else {
-                        this.moreExists = false;
-                    }
-
-                    response.data.data.data.forEach(data => {
-                        this.productInfo.push(data);
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-                .finally(() => {
-                    this.searchProduct = false;
-                });
-        }
+    goToDetail(proId) {
+      this.$router.push({ name: "detail", params: { id: proId } });
     },
-    computed: {},
-    created() {
-        EventBus.$on("search-value", value => {
-            this.searchvalue = value;
+    loadProduct: async function () {
+      try {
+        let response = await axios.post(`/api/product`, {
+          search: this.searchvalue,
+          category: this.categoryvalue,
+          lastId: this.lastProductId,
         });
-        EventBus.$on("category-value", value => {
-            this.categoryvalue = value;
-        });
-        this.loadMore();
-    }
+        this.productInfo.push.apply(this.productInfo, response.data.data);
+        this.lastProductId = this.productInfo[
+          this.productInfo.length - 1
+        ].item_id;
+        return response.data.data.length;
+
+        // if (
+        //     response.data.data.current_page <
+        //     response.data.data.last_page
+        // ) {
+        //     this.moreExists = true;
+        //     this.nextPage = response.data.data.current_page + 1;
+        // } else {
+        //     this.moreExists = false;
+        // }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    addToCart(id) {
+      if (_.isEmpty(User.data)) {
+        this.$router.push({ name: "login" });
+      } else {
+        this.$root.$refs.Loading.show();
+
+        axios
+          .get(window.Global.baseUrl + "/api/add/to/cart/" + id)
+          .then((resp) => {
+            this.$root.$refs.Nav.cartCounter = resp.data;
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+          .finally(() => {
+            this.$root.$refs.Loading.hide();
+          });
+      }
+    },
+    loadMore: async function () {
+      this.loading = true;
+      let dataLength = await this.loadProduct();
+      console.log(dataLength);
+      if (dataLength < 15) {
+        this.moreExists = false;
+      }
+      this.loading = false;
+
+      //   await axios
+      //     .get(`/api/product?page=` + this.nextPage)
+      //     .then((response) => {
+      //       console.log(response);
+
+      //       response.data.data.data.forEach((data) => {
+      //         this.productInfo.push(data);
+      //       });
+      //     })
+      //     .catch((err) => {
+      //       console.log(err);
+      //     })
+      //     .finally(() => {
+      //       this.loading = false;
+      //     });
+    },
+    filteredProduct() {
+      this.searchProduct = true;
+      this.productInfo = [];
+      this.lastProductId = 0;
+      this.loadProduct();
+      //   axios
+      //     .get(`/api/search?name=` + this.seachvalue)
+      //     .then((response) => {
+      //       if (response.data.data.current_page < response.data.data.last_page) {
+      //         this.moreExists = true;
+      //         this.nextPage = response.data.data.current_page + 1;
+      //       } else {
+      //         this.moreExists = false;
+      //       }
+
+      //       response.data.data.data.forEach((data) => {
+      //         this.productInfo.push(data);
+      //       });
+      //     })
+      //     .catch((err) => {
+      //       console.log(err);
+      //     })
+      //     .finally(() => {
+      //       this.searchProduct = false;
+      //     });
+    },
+  },
+  computed: {},
+  created() {
+    EventBus.$on("search-value", (value) => {
+      this.searchvalue = value;
+    });
+    EventBus.$on("category-value", (value) => {
+      this.categoryvalue = value;
+    });
+    this.loadProduct();
+  },
 };
 </script>
