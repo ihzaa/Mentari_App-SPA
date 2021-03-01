147a70ed4134894ca88ad76914b25474b3342dfa
ulfanuroktaviana575 <ulfanuroktaviana575@webmail.umm.ac.id>
2021-03-02 01:17:58 +0700

    fix deploy

--- resources/js/global.js
+++ resources/js/global.js
@@ -1,7 +1,7 @@
 export default
 {
-    // imgPath: 'https://mentari.admin.dzulumat.my.id/storage/',
-    // baseUrl: 'https://mentari.dzulumat.my.id'
-    imgPath: 'http://localhost:8080/mentari-app/public/storage/',
-    baseUrl: 'http://localhost:8000'
+    imgPath: 'https://mentari.admin.dzulumat.my.id/storage/',
+    baseUrl: 'https://mentari.dzulumat.my.id'
+    // imgPath: 'http://localhost:8080/mentari-app/public/storage/',
+    // baseUrl: 'http://localhost:8000'
 }
