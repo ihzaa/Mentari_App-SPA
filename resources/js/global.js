79803a234bc40df39e30a6cf605081f122a83fd7
yusufrnp <yusufrnp@gmail.com>
2021-03-01 22:01:31 +0700

    gatau yusuf

--- resources/js/global.js
+++ resources/js/global.js
@@ -1,5 +1,7 @@
 export default
 {
-    imgPath: 'https://mentari.admin.dzulumat.my.id/storage/',
-    baseUrl: 'https://mentari.dzulumat.my.id'
+    // imgPath: 'https://mentari.admin.dzulumat.my.id/storage/',
+    // baseUrl: 'https://mentari.dzulumat.my.id'
+    imgPath: 'http://localhost:8080/storage/',
+    baseUrl: 'http://localhost:8000'
 }
