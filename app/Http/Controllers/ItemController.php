49c7bdb96ce6fc7b61a4ab40eb7318b5f3623a8f
yusufrnp <yusufrnp@gmail.com>
2021-03-01 23:16:47 +0700

    fix cart kalo kelebihan ya dikurangi

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -27,15 +27,18 @@ class ItemController extends Controller
         } else {
             $itemInCart = cart::where('item_id', $id)->where('user_id', $user->id)->where('status', '0')->first();
             if (!empty($itemInCart)) {
-                $itemInCart->quantity++;
-                $itemInCart->save();
-                return response()->json(cart::where('user_id', $user->id)->where('status', '0')->count());
+                if ($item->stock < $itemInCart->quantity + 1) {
+                    return response()->json(cart::where('user_id', $user->id)->where('status', '0')->count());
+                } else {
+                    $itemInCart->quantity++;
+                    $itemInCart->save();
+                    return response()->json(cart::where('user_id', $user->id)->where('status', '0')->count());}
             } else {
                 cart::create([
                     'quantity' => '1',
                     'status' => '0',
                     'item_id' => $id,
-                    'user_id' => $user->id
+                    'user_id' => $user->id,
                 ]);
                 return response()->json(cart::where('user_id', $user->id)->where('status', '0')->count());
             }
@@ -44,14 +47,22 @@ class ItemController extends Controller
 
     public function getCartItem()
     {
-        $availableItem = item::where('stock', ">", "0")->pluck('id');
-        cart::whereNotIn('item_id', $availableItem)->where('user_id', Auth::user()->id)->delete();
+        $availableItem = item::where('stock', ">", "0")->pluck('stock', 'id')->toArray();
+        cart::whereNotIn('item_id', array_keys($availableItem))->where('user_id', Auth::user()->id)->where('status', "0")->delete();
+        $items = cart::where('user_id', Auth::user()->id)->where('status', "0")->get(['id', 'quantity', 'item_id'])->keyBy('id');
+        foreach ($items as $id => $item) {
+            if ($item->quantity > $availableItem[$item->item_id]) {
+                cart::find($id)->update([
+                    "quantity" => $availableItem[$item->item_id],
+                ]);
+            }
+        }
         $items = DB::select(DB::raw('SELECT c.id as "cart_id",i.id as "item_id",c.quantity, i.name, i.price, i.stock as "stock", (SELECT item_images.path FROM item_images WHERE item_images.item_id = i.id LIMIT 1) as img FROM carts as c JOIN items as i ON c.item_id=i.id WHERE c.user_id="' . Auth::user()->id . '" AND c.status="0"'));
 
         $addresses = address::where('user_id', Auth::user()->id)->get();
         return response()->json([
             "items" => $items,
-            "addresses" => $addresses
+            "addresses" => $addresses,
         ]);
     }
 
@@ -68,7 +79,7 @@ class ItemController extends Controller
     public function changeQuantity(Request $request)
     {
         cart::find($request->id)->update([
-            'quantity' => $request->quantity
+            'quantity' => $request->quantity,
         ]);
         return response()->json("ok");
     }
@@ -78,11 +89,11 @@ class ItemController extends Controller
         transaction::create([
             "address_id" => $request->address,
             "cart_id" => json_encode($request->items),
-            "user_id" => Auth::user()->id
+            "user_id" => Auth::user()->id,
         ]);
 
         cart::whereIn('id', $request->items)->update([
-            'status' => "1"
+            'status' => "1",
         ]);
 
         $cart = cart::whereIn('id', $request->items)->pluck('quantity', 'item_id');
